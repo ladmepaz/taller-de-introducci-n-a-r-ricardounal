5c86a551c1ef42c38e18515bcbe8c46a2be2f8db
ricardounal <rimedinac@unal.edu.co>
2024-10-28 17:26:20 -0500

    Update ejercicio01.R

--- ejercicio01.R
+++ ejercicio01.R
@@ -1,10 +1,6 @@
-# Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
-
-solucion <- function() {
-  # Asigna valores a 'a' y 'b' de manera que el valor asignado a 's' sea 100
-  a <- 50
-  b <- 50
-  s <- a+b
-  return(s)
+solucion- function (a,b ){
+    s<- a + b 
+    return (s)
 }
+solucion (50, 50)
 
