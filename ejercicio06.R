9d6e4927bcab77b71a5a3ec429612ab252b961e5
ricardounal <rimedinac@unal.edu.co>
2024-10-30 08:59:26 -0500

    Update ejercicio06.R

--- ejercicio06.R
+++ ejercicio06.R
@@ -1,15 +1,14 @@
-# Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
-
 solucion <- function() {
   # Ordena las asignaciones de manera que se devuelva un valor de 's' igual a 43
   s <- NULL
   a <- NULL
   b <- NULL
+  a <- 10
+  b <- 20
   s <- a+b
   a <- 8
   s <- s+a
-  b <- 20
   s <- s+5
-  a <- 10
   return(s)
 }
+solucion()
