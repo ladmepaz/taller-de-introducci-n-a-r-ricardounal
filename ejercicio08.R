09547a8e395e1d3d576303f1e081dbaf57c1f9f8
ricardounal <rimedinac@unal.edu.co>
2024-10-30 09:27:14 -0500

    Update ejercicio08.R

--- ejercicio08.R
+++ ejercicio08.R
@@ -1,15 +1,13 @@
-# Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
-
+# La función quedaría así
 solucion <- function() {
-  # Ingresa valores en las líneas indicadas de manera que a 's' se le asigne 4
-  # y a 't' se le asigne 10
   a <- 4
   b <- 8
   c <- 5
   d <- 3
-  x <- NULL # ingresa valor aquí
-  y <- NULL # ingresa valor aquí
-  s <- a*x-b
-  t <- 2*s+c-d
-  return(c(s,t,x,y))
+  x <- 3 # Valor encontrado
+  y <- NULL # No se necesita modificar
+  s <- a * x - b
+  t <- 2 * s + c - d
+  return(c(s, t, x, y))
 }
+solucion()
