ccb5d22ce7d30ecf8ff66b9cd768b42fa23a0f79
ricardounal <rimedinac@unal.edu.co>
2024-10-30 08:37:27 -0500

    Update ejercicio03.R
    hecho

--- ejercicio03.R
+++ ejercicio03.R
@@ -1,9 +1,7 @@
-# Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
-
-solucion <- function() {
-  # Asigna un valor a 'a' de manera que el valor asignado a 's' sea 5
-  a <- NULL # asigna valor aquí
-  b <- 20  # asigna valor aquí
+solucion <- function(a,b){
   s <- a-b
   return(s)
 }
+a<-25
+b<-20
+solucion(a,b)
