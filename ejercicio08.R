50bd05e004ca29f9ec0faa815ef51de741ed8d24
ricardounal <rimedinac@unal.edu.co>
2024-10-31 15:31:20 -0500

    Update ejercicio08.R

--- ejercicio08.R
+++ ejercicio08.R
@@ -1,13 +1,13 @@
 # La función quedaría así
 solucion <- function() {
-  a <- 4
-  b <- 8
-  c <- 5
-  d <- 3
-  x <- 3 # Valor encontrado
-  y <- NULL # No se necesita modificar
-  s <- a * x - b
-  t <- 2 * s + c - d
+  a<-4
+  b<-8
+  c<-5
+  d<-3
+  x<-3 # Valor encontrado
+  y<-NULL # No se necesita modificar
+  s<-a * x - b
+  t <-2*s+c-d
   return(c(s, t, x, y))
 }
 
