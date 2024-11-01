71e3fa6e99644de29cdc6b4ca87d7d94f11edf44
ricardounal <rimedinac@unal.edu.co>
2024-11-01 13:25:55 -0500

    Update ejercicio07.R

--- ejercicio07.R
+++ ejercicio07.R
@@ -1,6 +1,6 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
-solucion <- function() {
+solucion <-function(){
   # Escribe una expresión de manera que a 's' se le asigne el promedio de 'a', 'b', 'c', y 'd'
   a<-4
   b<-8
